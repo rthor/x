4ecde08cca02d8769451226ee4ce82bcf23b8014
Ragnar Þór Valgeirsson <ragnar.valgeirsson@gmail.com>
2013-11-11 12:42:10 +0000

    Version 0.0.3

--- src/intro.js
+++ src/intro.js
@@ -6,7 +6,7 @@ var root = this;
 var X = typeof exports !== 'undefined' ? exports : root.X = {};
 
 // Current version.
-X.Version = '0.0.2';
+X.Version = '0.0.3';
 
 // Use restful routes.
 X.restful = true;
